998712af0d9d5570b111eb8acc90833d7fad7174
Amy <77540881+AmySofia1451@users.noreply.github.com>
2024-11-09 16:53:24 -0500

    GA4: task 2, completed through SkillController. Testing now.

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,30 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+    @NotBlank(message = "Location is required")
+    @Size(min = 2, max = 50, message = "Location must be between 2 and 50 characters")
+    private String location;
+
+    public Employer () {}
+
+    public Employer(String location) {
+        this.location = location;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+
+
 }
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,23 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
+
+    //@NotNull?
+    @Size(max = 500, message = "Description too long!")
+    private String description;
+
+    public @Size(max = 500, message = "Description too long!") String getDescription() {
+        return description;
+    }
+
+    public void setDescription(@Size(max = 500, message = "Description too long!") String description) {
+        this.description = description;
+    }
+
+    public Skill () {}
 
 }
